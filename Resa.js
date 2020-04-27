c02892db94be6d2b5e8454eeb2c13fcef3e6be3d
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-04-27 12:57:41 +0100

    card counting function

---
+++ Resa.js
@@ -0,0 +1,29 @@
+// a card counting function
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch (card){
+  case 2:  case 3:  case 4:  case 5:  case 6:
+  count++;
+  break;
+
+//Since 7,8,9 are not worth anything, you ignore them
+
+  case 10: case "J": case "Q": case "K": case "A":
+  count--;
+  break;
+}
+
+if (count > 0){
+  return count + " Bet";
+} else if (count < 0) {
+  return count + " Hold";
+} else { 
+  return count + " Hold";
+}
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
