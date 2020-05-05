718b14f201faf2f307e49b467d3099077b75f2ea
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-05-05 18:03:55 +0100

    Regex Modification

--- usernameChecker.js
+++ usernameChecker.js
@@ -21,3 +21,17 @@ Change the regex userCheck to fit the constraints listed above.
 let username = "JackOfAllTrades";
 let userCheck = /^[a-z]([0-9][0-9]+|[a-z]+\d*)$/i; // Change this line
 let result = userCheck.test(username);
+
+
+/*
+Fix the regex so that it checks for the names of Franklin Roosevelt or 
+Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
+
+Then fix the code so that the regex that you have created is checked against myString and either true 
+or false is returned depending on whether the regex matches.
+*/
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
